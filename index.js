f67d7b854a58f905556a68f42fd04dddeb830ff8
Aliramazanian <ali.ramazanian1971@gmail.com>
2023-01-05 12:41:04 +0100

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ATTENTION: Die Funktionen werden über den Button in der HTML ausgeführt. Um unterschiedliche Funktionen zu probieren muss der Name der Funktion in der HTML bei onclick="[Funktionsname()]" eingetragen werden (Zeile 15 index.html).
-​
+
 // IMPORT
-​
+
 // VARIABLES
 const container = document.getElementById("container");
 const body = document.querySelector("body");
@@ -11,17 +11,17 @@ const nameTwo = document.getElementById("nameTwo");
 const submit = document.getElementById("submit");
 // EVENTS
 //______ Bubbling - wenn mehrere Events auf verschachtelten Elementen liegen werden die Events nacheinander ausgeführt.
-​
+
 // Eventlistener on Container, containerFarbe soll sich ändern
 // Eventlistener on box1, box1 Farbe soll sich ändern
-​
+
 //______ Delegation - parent ein Event geben und über event.target die kinder auswählen
 // Eventlistener auf container, Farbe des target soll sich ändern.
-​
+
 // STYLE INJECTING mit Object.assign([element].style,[ObjectWithStyles]);
-​
+
 // FUNCTIONS
-​
+
 // NASA API ASTRO PICTURE OF THE DAY
 const apod = async () => {
  try {
@@ -34,7 +34,7 @@ const apod = async () => {
  } catch (error) {
   console.log(error);
  }
-};​
+};
 // RANDOM QUOTE API
 const motivation = async () => {
  const options = {
@@ -43,14 +43,14 @@ const motivation = async () => {
    "X-RapidAPI-Key": "cc112b0b2cmshfb87ac2a691ba48p1f9360jsna08324b55413",
    "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
   },
- };​
+ };
  fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
   .then((response) => console.log(response.json()))
   .then((response) => {
    quote.innerText = `${response.content} -- ${response.originator.name}`;
   })
   .catch((err) => console.error(err));
-};​
+};
 // LOVERS CALCULATOR API
 const loversUpdate = () => {
  const options = {
@@ -59,9 +59,9 @@ const loversUpdate = () => {
    "X-RapidAPI-Key": "cc112b0b2cmshfb87ac2a691ba48p1f9360jsna08324b55413",
    "X-RapidAPI-Host": "love-calculator.p.rapidapi.com",
   },
- };​
+ };
  let nameOneValue = nameOne.value;
- let nameTwoValue = nameTwo.value;​
+ let nameTwoValue = nameTwo.value;
  // testen, ob namen eingegeben wurden, und ob sie nur aus Buchstaben bestehen
  if (
   nameOneValue.length > 0 &&
@@ -82,7 +82,7 @@ const loversUpdate = () => {
  } else {
   window.alert("You have to use letters only");
  }
-};​
+};
 // WEATHER API BY API NINJAs
 const weather = async () => {
  const options = {
@@ -91,7 +91,7 @@ const weather = async () => {
    "X-RapidAPI-Key": "cc112b0b2cmshfb87ac2a691ba48p1f9360jsna08324b55413",
    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
   },
- };​
+ };
  const data1 = await fetch(
   `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${nameOne.value}`,
   options
